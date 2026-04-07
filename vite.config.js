b698364fdf7c73895e7bf83ffdc3b705e719b55a
Alex <alex@cliphair.co.uk>
2026-04-07 12:33:49 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/clipahir-scoping-tool/',
   plugins: [react()],
   server: {
     host: true,
