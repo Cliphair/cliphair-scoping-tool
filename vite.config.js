cd9055a90b4b37510f79316e1aaae2536d52f06b
Alex <alex@cliphair.co.uk>
2026-04-07 12:43:58 +0100

    Correct base URL

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/clipahir-scoping-tool/',
+  base: '/cliphair-scoping-tool/',
   plugins: [react()],
   server: {
     host: true,
